b6e0a4aae65505f95a5af1b9de02d37f87d6cbae
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-08-11 14:29:51 +0300

    some fix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: [
+      'http://localhost:5173', // локальная разработка (Vite)
+      'https://cebanoleksandr.github.io/ai-planner-assistant-fe', // прод-домен фронта
+    ],
+    credentials: true, // если используешь cookies/сессии
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
